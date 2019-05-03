7088be1bcfd1a355d2d7582314eadd72e1458cd5
Tae Hyun Kim <taehyunkim@86.113.254.198.naturalwireless.com>
2019-05-03 01:45:46 -0400

    implemented feature to generate separate csv files

--- test.py
+++ test.py
@@ -1,18 +1,39 @@
 import re
 import urllib.request
-myfile = urllib.request.urlopen('http://www.ksbc.kerala.gov.in/circular/prmtrlisthigh1218.txt')
+import pandas as pd
+myfile = urllib.request.urlopen('http://ksbc.kerala.gov.in/reports/may_june_july/RMCI.TXT')
 data = myfile.readlines()
 dict_list = []
+entire_list = []
+title_list = []
+t = '.*\s(?P<SERIAL_NO>[0-9]+)\s(?P<ITEM_CODE>[0-9][0-9][0-9][0-9][0-9][0-9]).*\s(?P<DESCRIPTION>[A-Z].*[A-Z]).*\s(?P<MAX_AMOUNT>[0-9]+[.][0-9][0-9]).*'
+t1 = '.*\s(?P<REGION>[A-Z][A-Z][A-Z])\s(?P<TOWN>[A-Z]*)\s.*\s(?P<DATE>[A-Z][A-Z][A-Z]\s[0-9][0-9][0-9][0-9]).*'
+t2 = '.*\'(?P<CODE>[0-9][0-9][0-9])\s(?P<DISTILLERY>[A-Z].*[A-Z])\s'
 for line in data:
-    match = re.match('.*\s(?P<SLNO>[0-9]+)\s(?P<IC>[0-9][0-9][0-9][0-9][0-9][0-9]).*\s'
-                     '(?P<DESC>[A-Z].*[A-Z]).*\s(?P<MQ>[0-9]+[.][0-9][0-9]).*', str(line))
+    #print(line)
+    match = re.match(t2, str(line))
+    entire_list.append(line)
     if match:
+        new_dict = match.groupdict()
+        """
         new_dict = dict()
         new_dict['SERIAL.NO'] = match.group('SLNO')
         new_dict['ITEM_CODE'] = match.group('IC')
         new_dict['DESCRIPTION'] = match.group('DESC')
         new_dict['MAX_AMOUNT'] = match.group('MQ')
-        dict_list.append(new_dict)
-for line in dict_list:
-    print(dict_list)
-print(len(dict_list))
+        """
+        title_list.append(new_dict)
+#for line in dict_list:
+#    print(dict_list)
+#for i in range (50):
+#    print(data[i])
+#df = pd.DataFrame(data)
+#if df.columns.values is None:
+#    print("As expected")
+#print(df.columns.values)
+
+for i in range(10):
+    print(title_list[i])
+#for i in range(10):
+#    print(dict_list[i])
+#print(len(title_list))
